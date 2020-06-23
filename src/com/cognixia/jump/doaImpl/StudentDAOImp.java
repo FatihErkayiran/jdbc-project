b0ba003b17bfe49eb4924fee9efd1608fc6c5cfd
Carson Jobe <cdjobe@gmail.com>
2020-06-23 15:26:16 -0500

    added update student

--- src/com/cognixia/jump/doaImpl/StudentDAOImp.java
+++ src/com/cognixia/jump/doaImpl/StudentDAOImp.java
@@ -38,9 +38,29 @@ public class StudentDAOImp implements StudentDAO {
 
 	@Override
 	public boolean updateStudent(Student student) {
-		// TODO Auto-generated method stub
+		// cols: first_name, last_name, gender, date_of_birth, credits, address_id, dept_id
+
+		try(PreparedStatement pstmt = conn.prepareStatement("update student set first_name = ?, last_name = ?, gender = ?, date_of_birth = ?, credits = ?, address_id = ?, dept_id = ?");) {
+			pstmt.setString(1, student.getFirstName());
+			pstmt.setString(2, student.getLastName());
+			pstmt.setString(3, student.getGender());
+			pstmt.setDate(4, student.getDob());
+			pstmt.setInt(5, student.getCredits());
+			pstmt.setInt(6, student.getAddress().getId());
+			pstmt.setInt(7, student.getDept().getId());
+			
+			int update = pstmt.executeUpdate();
+			
+			if (update > 0) {
+				return true;
+			}
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}
+
 		return false;
 	}
+
 
 	@Override
 	public boolean deleteStudent(int id) {
